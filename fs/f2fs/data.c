c91e2deb86f434b2529421a709cbb600042008a0
Chao Yu <yuchao0@huawei.com>
2018-05-23 22:25:09 +0800

    f2fs: detect synchronous writeback more earlier
    This patch changes to detect synchronous writeback more earlier before,
    in order to avoid unnecessary page writeback before exiting asynchronous
    writeback.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: khusika <khusikadhamar@gmail.com>
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1686,6 +1686,13 @@ retry:
 			struct page *page = pvec.pages[i];
 			bool submitted = false;
 
+			/* give a priority to WB_SYNC threads */
+			if (atomic_read(&F2FS_M_SB(mapping)->wb_sync_req) &&
+					wbc->sync_mode == WB_SYNC_NONE) {
+				done = 1;
+				break;
+			}
+
 			done_index = page->index;
 retry_write:
 			lock_page(page);
@@ -1740,9 +1747,7 @@ continue_unlock:
 				last_idx = page->index;
 			}
 
-			/* give a priority to WB_SYNC threads */
-			if ((atomic_read(&F2FS_M_SB(mapping)->wb_sync_req) ||
-					--wbc->nr_to_write <= 0) &&
+			if (--wbc->nr_to_write <= 0 &&
 					wbc->sync_mode == WB_SYNC_NONE) {
 				done = 1;
 				break;
